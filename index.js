db331761ef0f29a6202867d9d3aaa9c4776105ce
Faiza Imtiaz <imtiazfaiza94@gmail.com>
2018-10-06 16:43:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,15 @@ function destructivelyRemoveFirstKitten(name){
   kittens.shift(name)
 }
 function appendKitten(name) {
-  kittens.concat(name)
+  return [...kittens, name]
 }
+ function prependKitten(name) {
+  return [name, ...kittens]
+}
+ function removeFirstKitten() {
+  return kittens.slice(1)
+}
+ function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
